5b786913bbcd29a23268d294035cf0d7a25f8964
akmal-2004 <akmallatifov2004@gmail.com>
2025-10-23 18:12:24 +0500

    Update down_revision in migration script to reflect correct lineage

--- backend/migrations/versions/3231e64230a3_add_cost_to_execution_result.py
+++ backend/migrations/versions/3231e64230a3_add_cost_to_execution_result.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '3231e64230a3'
-down_revision: Union[str, Sequence[str], None] = 'cb5cec1b033d'
+down_revision: Union[str, Sequence[str], None] = 'd489e2f7e044'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
